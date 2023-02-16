a77d42498764147ebbbb765c7c96baccc8acd897
Matthew Fitzgerald <mattlol85@gmail.com>
2023-02-16 17:11:28 -0500

    Removed Defaults, replaced with basic text

---
+++ src/Homepage.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import "./Homepage.css";
+const HomePage = () => {
+
+  return (
+    <div>
+      <h1>Fitz-Net</h1>
+      <h2>Under Construction</h2>
+    </div>
+  );
+};
+
+export default HomePage;
